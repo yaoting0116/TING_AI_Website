d1daef78f430eb244b8e42b1e8d1fd7985bf3ed5
朝霧 <102600436+yaoting0116@users.noreply.github.com>
2025-11-28 13:43:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, url_for
 import os
+from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
 
-# 全域設定 Jinja2 自訂變數分隔符（如果你確定要使用 {{% ... %}}）
-app.jinja_env.variable_start_string = '{{%'
-app.jinja_env.variable_end_string = '%}}'
+# Configure custom Jinja2 environment
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+env.variable_start_string = '{{%'
+env.variable_end_string = '%}}'
+env.globals.update(url_for=url_for)  # Add url_for to the Jinja2 environment
 
 @app.route('/')
 def index():
@@ -19,9 +23,9 @@ def music():
     music_folder = os.path.join(app.static_folder, 'music')
     music_files = [f for f in os.listdir(music_folder) if os.path.isfile(os.path.join(music_folder, f))]
     music_info = [{'url': url_for('static', filename='music/' + file), 'name': file} for file in music_files]
-    return render_template('music.html', music_info=music_info)
+    template = env.get_template('music.html')
+    return template.render(music_info=music_info)
 
-# 其他路由同上...
 @app.route('/game')
 def game():
     image_folder = os.path.join(app.static_folder, 'images')
@@ -29,6 +33,19 @@ def game():
     image_info = [{'url': url_for('static', filename='images/' + file), 'name': file} for file in image_files]
     return render_template('game.html', image_info=image_info)
 
+@app.route('/learning')
+def learning():
+    image_folder = os.path.join(app.static_folder, 'images')
+    image_files = [f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
+    image_info = [{'url': url_for('static', filename='images/' + file), 'name': file} for file in image_files]
+    return render_template('learning.html', image_info=image_info)
+
+@app.route('/NLP')
+def NLP():
+    image_folder = os.path.join(app.static_folder, 'images')
+    image_files = [f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
+    image_info = [{'url': url_for('static', filename='images/' + file), 'name': file} for file in image_files]
+    return render_template('NLP.html', image_info=image_info)
+
 if __name__ == '__main__':
-    # 本地測試用：production 時由 gunicorn 啟動，不需要 debug=True
-    app.run(host='0.0.0.0', debug=False)
+    app.run(debug=True)
